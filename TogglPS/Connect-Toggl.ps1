06e54162e21cef00017c6e864013b0bca3aad5e6
Phil Spokas <phil@intellitect.com>
2017-08-05 17:02:24 -0700

    PowerShell scripts that interact with Toggl

---
+++ TogglPS/Connect-Toggl.ps1
@@ -0,0 +1,126 @@
+﻿#
+# Toggl wrapper functions
+# 
+# todo: individual wrapper functions with unit testing
+# todo: Set-TglProject, 
+# todo: Set-TglClient???  
+# todo: Add-TglTime 2hr -Project -Task "" 
+#
+# https://github.com/toggl/toggl_api_docs/blob/master/chapters/time_entries.md
+# 
+
+$global:TglInitialized = $false
+[DateTimeOffset]$global:TglDate = Get-Date
+$global:TglMe = $null
+$global:TglWorkspace = $null
+$global:TglProjectID = 2655186   # default Admin project id
+
+$username = 'phil@intellitect.com'
+$password = '***'
+$apitoken = 'b842531c68f5f20ccc091803d5584140'
+
+$reportUri = 'https://toggl.com/reports/api/v2/details'
+$projectsUri = 'https://www.toggl.com/api/v8/projects'
+
+$headers = @{
+    'Authorization' = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($apitoken + ':api_token'));
+    'Content-Type' = 'application/json'
+}
+
+Function Initialize-Toggl() {
+    # get info about me
+    $aboutMeUri = 'https://www.toggl.com/api/v8/me'
+    $global:TglMe = Invoke-RestMethod -Uri $aboutMeUri -Method Get -Headers $headers
+
+    # get workspaces (if only one, then this is my workspace)
+    # todo handle multiple
+    $workspacesUri = 'https://www.toggl.com/api/v8/workspaces'
+    $global:TglWorkspace = Invoke-RestMethod -Uri $workspacesUri -Method Get -Headers $headers
+
+    # key info:
+    #   user id $me.data.id
+    #   workspace id $ws.id
+
+    $global:TglInitialized = $true
+}
+
+Function Get-TglDate() {
+    return $global:TglDate
+}
+
+Function Set-TglDate([DateTimeOffset]$NewDate ) {
+    $global:TglDate = $NewDate
+}
+
+Function Get-TglTimeEntries() {
+    # GET "https://www.toggl.com/api/v8/time_entries?start_date=2013-03-10T15%3A42%3A46%2B02%3A00&end_date=2013-03-12T15%3A42%3A46%2B02%3A00"
+    # dates must be ISO 8601
+    $timeEntriesUri = 'https://www.toggl.com/api/v8/time_entries'
+    return Invoke-RestMethod -Uri $timeEntriesUri -Method Get -Headers $headers
+}
+
+Function Get-TglClients() {
+    #todo get workspace if not gotten
+    $workspaceClientsUri = 'https://www.toggl.com/api/v8/workspaces/'+$TglWorkspace.id+'/clients'
+    $Tglclients = Invoke-RestMethod -Uri $workspaceClientsUri -Method Get -Headers $headers
+    $TglClients
+}
+
+Function Get-TglProjects() {
+    # todo figure out types, if I filter the project list, I lost type info as in
+    #  foreach ($p in ($projects.cid -eq 884250))  { $p, $p.GetType() }
+    $workspaceProjectsUri = 'https://www.toggl.com/api/v8/workspaces/'+ $TglWorkspace.id+'/projects'
+    $global:TglProjects = Invoke-RestMethod -Uri $workspaceProjectsUri -Method Get -Headers $headers
+    $global:TglProjects
+}
+
+Function Set-TglProject() {
+    # find project based on provided parameters
+    # set the project and return the id
+    return "not implemented"
+}
+
+Function Get-TglDetailedReport() {
+    $detailedReportUri = 'https://toggl.com/reports/api/v2/details?user_agent=phil@intellitect.com&workspace_id=' + $TglWorkspace.id
+    $report = Invoke-RestMethod -Uri $detailedReportUri -Method Get -Headers $headers
+    $report
+}
+
+
+Function Add-TglTimeEntry {
+    #todo parameters for project, customer, hours, date with defaults using preset values
+    param (
+      $ProjectID = $global:TglProjectID,
+      $Description = 'no description',
+      $Duration = 30,  # default time entry is 30 minutes
+      $Billable = $false,
+      [DateTimeOffset]$StartDate = $global:TglDate,
+      $Debug = $false
+    )
+
+    # Retain passed parameters as new defaults
+    $global:TglProjecID = $ProjectID
+    $global:TglDate = $StartDate
+
+    $addTimeEntryUri = "https://www.toggl.com/api/v8/time_entries"
+    $timeEntryDetails = @{
+        'wid' = $global:TglWorkspace.id;
+        'pid' =  $ProjectID;
+        'description' = $Description;
+        'duration' = $Duration*60;
+        'created_with' = 'powershell';
+        'start' = $StartDate.ToString('o'); # '2014-01-02T15:00:00Z'; # 
+        'billable' = $Billable;
+    }
+    $timeEntry = @{'time_entry' = $timeEntryDetails }
+
+    if ($Debug -eq $true) {
+        $timeEntryDetails | fl
+    }
+    else {
+        $result = Invoke-RestMethod -Uri $addTimeEntryUri -Method Post -Headers $headers -Body (ConvertTo-Json $timeEntry)
+        $result
+    }
+}
+
+
---
+++ TogglPS/scratch.ps1
@@ -0,0 +1,103 @@
+﻿
+  Id CommandLine                                                                                                                     
+  -- -----------                                                                                                                     
+   1 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+   2 $m3                                                                                                                             
+   3 $me                                                                                                                             
+   4 $m3.data                                                                                                                        
+   5 $me.data                                                                                                                        
+   6 $me.data.email                                                                                                                  
+   7 $me.data.id                                                                                                                     
+   8 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+   9 $projects                                                                                                                       
+  10 $projects[0]                                                                                                                    
+  11 $projects[0].name                                                                                                               
+  12 $projects                                                                                                                       
+  13 $projects | fl -Property Name                                                                                                   
+  14 $projects | ft -Property Name                                                                                                   
+  15 $projects | ft -Property id -Property Name                                                                                      
+  16 $projects                                                                                                                       
+  17 $projects | fl -Property id -Property Name                                                                                      
+  18 $projects | fl -Property id, Name                                                                                               
+  19 $projects | ft -Property id, Name                                                                                               
+  20 Date                                                                                                                            
+  21 Date.ToString()                                                                                                                 
+  22 Date                                                                                                                            
+  23 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  24 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  25 $timeEntry                                                                                                                      
+  26 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  27 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  28 $headers                                                                                                                        
+  29 $timeEntry                                                                                                                      
+  30 $timeEntryDetails                                                                                                               
+  31 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  32 $timeEntryDetails                                                                                                               
+  33 Invoke-RestMethod -Uri $addTimeEntryUri -Method Post -Headers $headers                                                          
+  34 Invoke-RestMethod -Uri $addTimeEntryUri -Method Post -Headers $headers -Body $timeEntry                                         
+  35 Invoke-RestMethod -Uri $addTimeEntryUri -Method Post -Headers $headers -Body $timeEntry                                         
+  36 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  37 $timeEntry | ConvertTo-Json                                                                                                     
+  38 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  39 $projects                                                                                                                       
+  40 $projects | fl -Property id, name                                                                                               
+  41 $projects | ft -Property id, name                                                                                               
+  42 $projects[0]                                                                                                                    
+  43 $projects | ft -Property id, name                                                                                               
+  44 Invoke-RestMethod -Uri $addTimeEntryUri -Method Post -Headers $headers -Body $timeEntry                                         
+  45 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  46 $ws.id                                                                                                                          
+  47 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  48 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  49 $projects | ft -Property id, name                                                                                               
+  50 projects | where-object ($_.name -eq 'USANA')                                                                                   
+  51 $projects | where-object ($_.name -eq 'USANA')                                                                                  
+  52 $projects                                                                                                                       
+  53 $projects.name                                                                                                                  
+  54 $projects | ? ($_.name -eq 'website')                                                                                           
+  55 $projects[0]                                                                                                                    
+  56 $projects | ? ($_.id -eq 7256589)                                                                                               
+  57 $projects.GetType()                                                                                                             
+  58 $projects | ft -Property id, name, billable                                                                                     
+  59 $projects | ? {$_.id -eq 7256589}                                                                                               
+  60 $projects | ? {$_.name -eq 'website'}                                                                                           
+  61 $projects | ? {$_.name -eq 'admin'}                                                                                             
+  62 $projects.Item(340635)                                                                                                          
+  63 $projects.Item(3406255)                                                                                                         
+  64 $projects                                                                                                                       
+  65 $projects | Select-Object id, cid, name                                                                                         
+  66 $projects | Select-Object id, cid, name | ? {$_.name -eq 'admin'}                                                               
+  67 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  68 C:\pdev\ps\Toggl\Connect-Toggl.ps1                                                                                              
+  69 $clients                                                                                                                        
+  70 $clents | ? {$_.name -eq 'Intellitect'}                                                                                         
+  71 $clents | ? {$_.name -eq 'IntelliTect'}                                                                                         
+  72 $clents | ? {$_.name -eq 'Hecla'}                                                                                               
+  73 $clients.Address()                                                                                                              
+  74 $clients.Item(0)                                                                                                                
+  75 $clients.Item('Hecla')                                                                                                          
+  76 $clients.Item(2)                                                                                                                
+  77 $clents | ? {$_.name -eq 'Hecla'}                                                                                               
+  78 $clents | ? {$_.name -eq 'IntelliTect'}                                                                                         
+  79 $clients                                                                                                                        
+  80 $clients.Where(id -eq 884250)                                                                                                   
+  81 $clients.Where( "id -eq 884250" )                                                                                               
+  82 $clients.Where( {id -eq 884250} )                                                                                               
+  83 $clients.Where( {$_.id -eq 884250} )                                                                                            
+  84 $clients.Where( {$_.name -eq 'Hecla'} )                                                                                         
+  85 $clients.Where( {$_.name -eq 'Intellitect'} )                                                                                   
+  86 $clients.Where( {$_.name -eq 'Intellitect '} )                                                                                  
+  87 $clients.Where( {$_.name -eq 'Intellitect'} )                                                                                   
+  88 $Projects.Where( {$_.name -like 'admin'} )                                                                                      
+  89 $Projects.Where( {$_.name -like 'admin'} ) | select id, name, cid                                                               
+  90 $Projects.Where( {$_.name -like 'admin'} ) | select id, name, cid                                                               
+  91 $Projects.Where( {$_.name -like 'admin'} ) | select id, name, $clients.where({$_.id = cid})                                     
+  92 $Projects.Where( {$_.name -like 'admin'} ) | select id, name, $clients.where({$_.id .eq cid})                                   
+  93 $Projects.Where( {$_.name -like 'admin'} ) | select id, name, $clients.where({$_.id -eq cid})                                   
+  94 $Projects.Where( {$_.name -like '*admin*'} )                                                                                    
+  95 $Projects.Where( {$_.name -like '*admin*'} ) | select name, id                                                                  
+  96 $Projects.Where({$_.name -like '*admin*'}) | select name, id                                                                    
+  97 $Projects.Where({$_.name -like '*admin*'}) | select name, id, cid                                                               
+  98 history                                                                                                                         
+
+
